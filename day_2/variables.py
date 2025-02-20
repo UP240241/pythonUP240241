f4aea77f61a3c471d182eb7b2ceede0ac9262cd3
UP240241 <up240241@alumnos.upa.edu.mx>
2025-02-20 13:34:37 -0600

    Día 2 de Python, v1.0

---
+++ day_2/variables.py
@@ -0,0 +1,41 @@
+#Día 2 de programación en Python
+apellido="Reséndiz"
+nombre_completo="Ismael Reséndiz Esquivel"
+pais="México"
+ciudad="Aguascalientes"
+edad=18
+año=2006
+is_married=False
+is_true=True
+is_light_on=True
+carrera, grupo, cuatrimestre="Ingenieniería Mecatrónica", "B", 1
+
+
+print(type(apellido))
+print(type(nombre_completo))    
+print(type(pais))
+print(type(ciudad))
+print(type(edad))
+print(type(año))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(carrera))
+print(type(grupo))
+print(type(cuatrimestre))
+
+print(len(nombre_completo)), print(len(apellido))
+print("Nombre:", nombre_completo,  "es mayor que", apellido) 
+
+
+num_uno=5
+num_dos=4
+variable_total=num_uno+num_dos
+diff=num_uno-num_dos
+producto=num_uno*num_dos
+division=num_uno/num_dos
+remainder=num_uno%num_dos
+exp=num_uno**num_dos
+floor_division=num_uno//num_dos
+area_del_circulo=3.14*(30**2)
+circunferencia_del_circulo=2*3.14*30
